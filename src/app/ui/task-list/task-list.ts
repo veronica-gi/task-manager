fc9a74c992a5737f9f1ff5cefafd0c594de1d20d
Verónica García <veronicagaiglesias@gmail.com>
2026-02-09 17:47:13 +0100

    feat: Add task creation and deletion to task list

--- src/app/ui/task-list/task-list.ts
+++ src/app/ui/task-list/task-list.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
@@ -14,5 +15,23 @@ export class TaskList {
     { id: 2, title: 'Create Task Manager', completed: true },
     { id: 3, title: 'Refactor core logic', completed: false },
   ];
+
+newTaskTitle = '';
+
+  addTask() {
+    if (!this.newTaskTitle.trim()) return;
+
+    this.tasks.push({
+      id: Date.now(),
+      title: this.newTaskTitle,
+      completed: false,
+    });
+
+    this.newTaskTitle = '';
+  }
+
+  deleteTask(id: number) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
 }
 
